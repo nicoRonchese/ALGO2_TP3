17e9fefadbbfb99ee1d856c4f53868f5de4bcbb1
LourRam <louramirezalmada@gmail.com>
2021-11-27 13:02:43 -0300

    Agrego Aserradero.h y Aserradero.cpp

---
+++ Aserradero.cpp
@@ -0,0 +1,18 @@
+#include "Aserradero.h"
+
+Aserradero::Aserradero(int jugador) : EdificioColocable(jugador) {
+    nombre = ASERRADERO;
+    signo_mapa = SIGNO_ASERRADERO;
+    vida = VIDA_EDIFICIO_DEBIL;
+}
+
+void Aserradero::mostrar(){
+ cout << "Soy un Aserradero y me encuentro en el casillero consultado." << endl;
+ cout << "Tengo " << vida << " de vida y mi propietario es el jugador " << propietario + 1 << endl;
+}
+
+void Aserradero::recolectar(){
+    cout << "un Aserradero produjo 25 de madera" << endl;
+}
+
+Aserradero::~Aserradero(){}
---
+++ Aserradero.h
@@ -0,0 +1,35 @@
+#ifndef ASERRADERO_H_INCLUDED
+#define ASERRADERO_H_INCLUDED
+#include "Edificio Colocable.h"
+
+class Aserradero : public EdificioColocable{
+
+ public:
+    /*
+    Constructor
+    * Pre: Recibe un jugador valido.
+    * Post: Construye un aserradero.
+    */
+    Aserradero(int jugador);
+
+    /*
+    * Pre: -.
+    * Post: Muestra por pantalla la vida y el propietario del aserradero.
+    */
+    void mostrar();
+
+    /*
+    * Pre: -.
+    * Post: Recolecta los materiales correspondientes al aserradero,
+    */
+    void recolectar();
+
+    /*
+    Destructor
+    * Pre: -.
+    * Post: Libera la memoria.
+    */
+    ~Aserradero();
+};
+
+#endif // ASERRADERO_H_INCLUDED
