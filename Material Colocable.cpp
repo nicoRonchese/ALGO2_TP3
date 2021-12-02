c7a72a9083f8237d69c43c30a091b034107db3d7
theojorge <92816076+theojorge@users.noreply.github.com>
2021-12-02 18:09:10 -0300

    Update Material Colocable.cpp

--- Material Colocable.cpp
+++ Material Colocable.cpp
@@ -1,10 +1,35 @@
 #include "Material Colocable.h"
 
-MaterialColocable::MaterialColocable(){}
+MaterialColocable::MaterialColocable(string nombre){
+ if (nombre==NOMBRE_PIEDRA){
+  this->nombre = nombre;
+  this->signo_mapa = SIGNO_PIEDRA;
+  this->cantidad = CANTIDAD_PIEDRA;
+ }
+ else if (nombre==NOMBRE_MADERA){
+  this->nombre = nombre;
+  this->signo_mapa = SIGNO_MADERA;
+  this->cantidad = CANTIDAD_MADERA;
+ }
+ else if (nombre==NOMBRE_METAL){
+  this->nombre = nombre;
+  this->signo_mapa = SIGNO_METAL;
+  this->cantidad = CANTIDAD_METAL;
+ }
+ else if (nombre==NOMBRE_ANDYCOIN){
+  this->nombre = nombre;
+  this->signo_mapa = SIGNO_ANDYCOIN;
+  this->cantidad = CANTIDAD_ANDYCOIN;
+ }
+}
 
-void MaterialColocable::mostrar(){}
+void MaterialColocable::mostrar(){
+ cout << "Soy un material del tipo "<<nombre<<" y estoy en el casillero consultado" << endl;
+}
 
-void MaterialColocable::mostrar_en_mapa(){}
+void MaterialColocable::mostrar_en_mapa(){
+ cout<<signo_mapa; //color
+}
 
 string MaterialColocable::mostrar_material(){
  return nombre;
